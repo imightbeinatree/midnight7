0d1dd23918af01b86cd2d1a1cf0c7cca16e993be
Michael Orr <michael@cloudspace.com>
2014-04-08 04:55:31 +0000

    initial project generation using the Cloudspace Rails 4 Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Midnight7::Application.config.secret_key_base = 'adf98d35533336c3e6d7a22616c48c89acfe7f1bab25416744e8749837d45d112d6b52da27b9b243ccab4396af82910fd7835f1e0a541e98aa8cd5fc4672a539'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Midnight7::Application.config.session_store :cookie_store, key: '_midnight7_session'
